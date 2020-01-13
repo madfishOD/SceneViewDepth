a2d362e02c59d220e2903281b1a821da1cc28dd1
madfishOD <madfish.od@gmail.com>
2020-01-13 21:15:47 +0200

    init

---
+++ SceneViewDepthProject/Assets/Editor/SceneViewDepth.cs
@@ -0,0 +1,70 @@
+﻿
+using UnityEditor;
+using UnityEditor.EditorTools;
+using UnityEngine;
+
+
+[EditorTool("SceneViewDepth")]
+public class SceneViewDepth : EditorTool
+{
+
+    public Shader _depthDebugShader;
+
+    private Camera      _sceneCamera;
+    private bool        _depthDebug;
+    private Material    _depthDebugMaterial;
+
+
+    private bool _toolStateIsActive;  
+
+    private void OnEnable()
+    {
+        EditorTools.activeToolChanged += OnActiveToolDidChange;
+        SceneView.duringSceneGui      += DuringSceneGui;
+        Camera.onPostRender           += PostRender;
+    }
+
+    private void OnDisable()
+    {
+        EditorTools.activeToolChanged -= OnActiveToolDidChange;
+        SceneView.duringSceneGui      -= DuringSceneGui;
+        Camera.onPostRender           -= PostRender;
+    }
+
+    private void OnActiveToolDidChange()
+    {
+        _toolStateIsActive = EditorTools.IsActiveTool(this);
+
+    }
+    //// All stuff running in scene view
+    private void DuringSceneGui(SceneView sceneView)
+    {
+        if (_sceneCamera == null && Camera.current != null)
+            _sceneCamera = Camera.current;
+    }
+    //// All stuff as postprocess-like effects include some debug goes here
+    private void PostRender(Camera cam)
+    {
+        if (!_toolStateIsActive)
+            return;
+        if (cam != _sceneCamera)        //Need to check if camera is scene camera to avoid some artifacts in inspector and other editor windows
+            return;             
+        DebugDrawDepth();           //Try to tweak scene camera far clip manualy if you see just black shiluettes
+    }
+
+    //// Draw fullscreen plane with depth texture using specifyied shader
+    void DebugDrawDepth()
+    {
+        if (_depthDebugShader == null)
+        {
+            Debug.LogError("_depthDebugShader is null! Check settings asset please");
+            return;
+        }
+        // Create new material instance if it's null
+        if (_depthDebugMaterial == null)
+            _depthDebugMaterial = new Material(_depthDebugShader);
+        // Proceduraly draw quad with depth texture
+        _depthDebugMaterial.SetPass(0);
+        Graphics.DrawProceduralNow( MeshTopology.Points, 1 );
+    }
+}
---
+++ SceneViewDepthProject/Assets/Shaders/GS_DepthDebug.shader
@@ -0,0 +1,92 @@
+﻿Shader "Unlit/GS_DepthDebug"
+{
+	Properties
+	{
+		_Color("", Color) = (1, 1, 1, 1)
+	}
+		SubShader
+	{
+	Tags{"Queue" = "Transparent+1" "IgnoreProjector" = "True" }
+
+	Pass
+	{
+	Name "Unselected Verts 0"
+	ColorMask RGBA
+		ZTest  Always
+		ZWrite Off
+		//Blend SrcAlpha OneMinusSrcAlpha
+
+		CGPROGRAM
+		#pragma target 5.0
+		#pragma vertex vert
+		#pragma fragment frag
+		#pragma geometry GS_Main
+		#include "UnityCG.cginc"
+
+		struct GS_input
+		{
+			float4 pos : POSITION;
+		};
+
+		struct FS_input
+		{
+			float4 pos : SV_POSITION;
+			float2 uv  : TEXCOORD0;
+		};
+
+		fixed4 _Color;
+		UNITY_DECLARE_DEPTH_TEXTURE(_CameraDepthTexture);
+
+		// Vertex Shader -------------------------------------------------------
+		GS_input vert()
+		{
+			GS_input o;
+			o.pos = float4(0.0, 0.0, 0.0, 1.0);
+			return o;
+		}
+
+		// Geometry Shader -----------------------------------------------------
+		[maxvertexcount(4)]
+		void GS_Main(point GS_input p[1], inout TriangleStream<FS_input> triStream)
+			{
+				float3 up    = float3(0.0, 1.0, 0.0);
+				float3 right = float3(1.0, 0.0, 0.0);
+				float  size  = 1.0;
+				
+				float4 v[4];
+				v[0] = float4(p[0].pos + size * right - size * up, 1.0f);
+				v[1] = float4(p[0].pos + size * right + size * up, 1.0f);
+				v[2] = float4(p[0].pos - size * right - size * up, 1.0f);
+				v[3] = float4(p[0].pos - size * right + size * up, 1.0f);
+
+				FS_input pIn;
+
+				pIn.pos = v[0];
+				pIn.uv  = float2(1, 1);
+				triStream.Append(pIn);
+
+				pIn.pos = v[1];
+				pIn.uv  = float2(1, 0);
+				triStream.Append(pIn);
+
+				pIn.pos = v[2];
+				pIn.uv  = float2(0, 1);
+				triStream.Append(pIn);
+
+				pIn.pos = v[3];
+				pIn.uv  = float2(0, 0);
+				triStream.Append(pIn);
+			}
+
+		// Fragment Shader -----------------------------------------------
+		float4 frag(FS_input i) : SV_Target
+		{
+			float depth = SAMPLE_DEPTH_TEXTURE(_CameraDepthTexture, i.uv);  // sample from depth texture
+			depth = Linear01Depth(depth);
+			return depth;
+		}
+
+		ENDCG
+		}
+	}
+}
